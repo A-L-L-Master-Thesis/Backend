de73734b356f6c7731c77bba3d8eaefe08d01b00
Alechj <alec@hotmail.dk>
2020-10-05 14:43:08 +0200

    wip: setup of initial socket service

---
+++ P9-Backend/Models/Message.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9_Backend.Models
+{
+    public class Message
+    {
+        public string sender { get; set; }
+        public string message { get; set; }
+        public string target { get; set; }
+
+        public Message(string sender, string message, string target)
+        {
+            this.sender = sender;
+            this.message = message;
+            this.target = target;
+        }
+
+        public byte[] toBytes()
+        {
+            var obj = JsonConvert.SerializeObject(this);
+            return Encoding.UTF8.GetBytes(obj);
+        }
+    }
+}
---
+++ P9-Backend/Services/SocketService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using P9_Backend.Models;
+
+namespace P9_Backend.Services
+{
+    public class SocketService : ISocketService
+    {
+        private Dictionary<string, TcpClient> _clientDict = new Dictionary<string, TcpClient>();
+
+        public SocketService()
+        {
+            Start();
+        }
+
+        public void Start()
+        {
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 44310);
+            listener.Start(10);
+
+            while (true)
+            {
+                IAsyncResult res = listener.BeginAcceptTcpClient(HandleIncommingConnection, listener);
+            }
+        }
+
+        private async void HandleIncommingConnection(IAsyncResult res)
+        {
+            TcpListener connectionListener = (TcpListener)res.AsyncState;
+            TcpClient connectionClient = connectionListener.EndAcceptTcpClient(res);
+
+            var data = new byte[connectionClient.ReceiveBufferSize];
+
+            using (NetworkStream ns = connectionClient.GetStream())
+            {
+                await ns.ReadAsync(data, 0, data.Length);
+            }
+
+            string msg = Encoding.UTF8.GetString(data);
+
+            try
+            {
+                Models.Message message = (Models.Message)JsonConvert.DeserializeObject(msg);
+                if (!_clientDict.ContainsKey(message.sender))
+                {
+                    _clientDict.Add(message.sender, connectionClient);
+                }
+
+                Console.WriteLine(message.message);
+            }
+            catch
+            {
+                return;
+            }
+        }
+    }
+}
