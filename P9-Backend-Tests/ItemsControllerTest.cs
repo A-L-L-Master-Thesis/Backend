e1bce4ef920fa79e7acc833408f197176fbac30b
HaZz <magnustboisen@gmail.com>
2020-10-13 11:02:40 +0200

    wip: drone service

---
+++ P9-Backend-Tests/ItemsControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P9_Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P9_Backend_Tests
+{
+    public class ItemsControllerTest
+    {
+        protected ItemsControllerTest(DbContextOptions<DbContext> contextOptions)
+        {
+            ContextOptions = contextOptions;
+
+            Seed();
+        }
+
+        protected DbContextOptions<DbContext> ContextOptions { get; }
+
+        private void Seed()
+        {
+            using (var context = new DbContext(ContextOptions))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                Position pos = new Position { ID = 1, Altitude = 22, Latitude = 55, Longitude = 56 };
+                Drone newDrone = new Drone { UUID= "test", Status= DroneStatus.Charging, BatteryPercentage=33, CurrentPosition=pos, LastUpdate=DateTime.Now, IP="1.1.1.1"};
+     
+                context.Add(newDrone);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ P9-Backend-Tests/Services/DroneServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using P9_Backend.Services;
+using System.Data.Common;
+
+namespace P9_Backend_Tests.Services
+{
+    public class DroneServiceTests : ItemsControllerTest
+    {
+        private readonly DbConnection _connection;
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        protected DroneServiceTests(DbContextOptions<DbContext> contextOptions) : base(
+            new DbContextOptionsBuilder<DbContext>()
+                .UseSqlite(CreateInMemoryDatabase())
+                .Options)
+        {
+            _connection = RelationalOptionsExtension.Extract(ContextOptions).Connection;
+        }
+
+        private static DbConnection CreateInMemoryDatabase()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+
+            connection.Open();
+
+            return connection;
+        }
+
+        public void Dispose() => _connection.Dispose();
+
+
+        [TestCase("test", ExpectedResult = QueryResult.OK)]
+        public QueryResult Test_DroneDelete(string uuid)
+        {
+            using (var context = new DbContext(ContextOptions))
+            {
+                DroneService service = new DroneService(_scopeFactory);
+
+                return service.DeleteDrone(uuid).Result;
+            }
+        }
+    }
+}
