fcc5c1326658b34aa4c14993f820cfec4bbc1ca8
Alechj <alec@hotmail.dk>
2021-03-09 12:36:09 +0100

    chore: Initial Demo script, WIP

---
+++ P9-Backend/Controllers/DemoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P9_Backend.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P9_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : Controller
+    {
+        private readonly IDemoService _demoService;
+        public DemoController(IDemoService demoService)
+        {
+            _demoService = demoService;
+        }
+
+        // POST: api/demo/(true/false)
+        [HttpPost("{demoRunning}")]
+        public async Task<ActionResult<bool>> ToggleDemo(bool demoRunning)
+        {
+            if (demoRunning)
+                return await _demoService.StartDemo();
+            else
+                return await _demoService.StopDemo();
+        }
+
+        // DELETE: api/demo
+        [HttpDelete]
+        public async Task<ActionResult<bool>> ResetDemo(string id)
+        {
+            return await _demoService.ResetDemo();
+        }
+    }
+}
